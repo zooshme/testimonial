f5fdc3f5ac7f1ebbc8f5b71b294709c581a2128c
Ovidiu Spatacian-Tarnu <contact@zoosh.me>
2015-05-01 20:53:51 +0100

    compiling hbs file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require("./template.hbs");
+var html = require("./template.hbs");
+var data = {
+  text: 'Donec id elit non mi porta gravida at eget metus. Sed posuere consectetur est at lobortis.',
+  author: {
+    name: 'Lorem Ipsum',
+    role: 'Dolor Sit',
+    company: 'Amet Inc.'
+  }
+}
+
+var str = html(data);
+module.exports = str;
